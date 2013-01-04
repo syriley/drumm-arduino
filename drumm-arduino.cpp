ed3ae53b7e10d7362a6195f16a8796d61fc834f7
Simon Riley <syriley@gmail.com>
2013-01-04 07:53:13 +0000

    bluetooth added back in

--- drumm-arduino.cpp
+++ drumm-arduino.cpp
@@ -1,5 +1,14 @@
 // Do not remove the include below
 #include "drumm-arduino.h"
+#include "SensorManager.h"
+#include <SoftwareSerial.h>
+
+const int rxPin = 2;
+const int txPin = 3;
+
+
+SensorManager sensorManager;
+SoftwareSerial bluetooth = SoftwareSerial(rxPin, txPin);
 
 
 //The setup function is called once at startup of the sketch
@@ -7,13 +16,30 @@ void setup()
 {
 	  Serial.begin(4800);
 	  Serial.println("Start Arduino");
-// Add your initialization code here
+
+	  SoftwareSerial bluetooth = SoftwareSerial(rxPin, txPin);
+	  bluetooth.begin(115200);
+	  bluetooth.println("Bluetooth Started");
 }
 
-// The loop function is called in an endless loop
 void loop()
 {
-	Serial.println("Looping");
-	delay(1000);
-//Add your repeated code here
+	//sensorManager.printStates();
+
+	int *states = sensorManager.getActiveStates();
+	Serial.println("");
+	if(states[0] == 1) {
+
+		bluetooth.begin(115200);
+		bluetooth.println();
+		for(int i = 1; i < 4; i++){
+			int state = states[i];
+			bluetooth.print(state);
+			bluetooth.print(":");
+			Serial.print(state);
+			Serial.print(":");
+		}
+
+	}
+	delay(10);
 }
